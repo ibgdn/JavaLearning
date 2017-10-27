a94ac0fda7587c9f2a9f4ee48433448e07b459b8
ibgdn <ibgdn4@gmail.com>
2017-10-27 18:14:25 +0800

    To complete stage one for SpringMVC project and add readme file.

--- SpringMVC/src/controller/IndexController.java
+++ SpringMVC/src/controller/IndexController.java
@@ -1,4 +1,21 @@
 package controller;
 
-public class IndexController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+/**
+    实现 Controller 接口，提供 handleRequest() 方法处理请求（处理 /index 的请求）
+    SpringMVC 通过 ModelAndView 对象把模型和视图结合在一起。
+ */
+public class IndexController implements Controller{
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView mav = new ModelAndView("index.jsp"); // 视图 index.jsp
+        mav.addObject("message","Hello SpringMVC"); // 模型数据 message，内容 Hello Spring MVC
+        return mav;
+    }
 }
--- SpringMVC/web/index.jsp
+++ SpringMVC/web/index.jsp
@@ -5,12 +5,13 @@
   Time: 9:56
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
 <html>
   <head>
     <title>$Title$</title>
   </head>
   <body>
+  <h1>${message}</h1>
   $END$
   </body>
 </html>
